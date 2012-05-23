c83abdf874996e8375204f6ef86940cf432b0508
Baz <the.ewok@gmail.com>
2012-05-22 20:22:57 -0600

    Adding comments

--- library/AutoDirectory.php
+++ library/AutoDirectory.php
@@ -1,10 +1,43 @@
 <?php
+	/**
+	* AutoDirectory
+	* Autoloaded Directory Listing
+	*
+	* @author Barry O'Mahony <the.ewok@gmail.com>
+	* @version 1.0
+	* @package Beatport Homework
+	*/
 
+	/**
+	* AutoDirectory
+	* Autoloaded Directory Listing
+	*
+	* @author Barry O'Mahony <the.ewok@gmail.com>
+	* @version 1.0
+	* @package Beatport Homework
+	* @subpackage classes
+	*/
 	class AutoDirectory {
-		
+	
+		/**
+		* Directory to scan
+		* @access public
+		* @var string
+		*/
 		public $directory = NULL;
+		
+		/**
+		* Scan results
+		* @access public
+		* @var array
+		*/
 		public $contents = array();
 		
+		/**
+		* Get the directory contents
+		* @access public
+		* @return array
+		*/
 		public function getDirContents() {
 			if($this->directory!==NULL) {
 				if(is_dir($this->directory)) {
